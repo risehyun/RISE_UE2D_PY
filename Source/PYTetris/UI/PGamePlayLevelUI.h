95ce1c17d48d51dd9f4032b2d294e71e964571b6
risehyun <risehyun01@gmail.com>
2024-02-10 10:37:13 +0900

    [FEAT/0210] GamePlay Level UI 초안 작성

---
+++ Source/PYTetris/UI/PGamePlayLevelUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PGamePlayLevelUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PYTETRIS_API UPGamePlayLevelUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
